06ff81aa731f721baa514a507b25b54859d1ed4b
shindeamul76 <shindeamul76@gmail.com>
2024-10-16 23:02:56 +0530

    fix: fetching keys from db

--- client/views.py
+++ client/views.py
@@ -49,7 +49,7 @@ class ClientCreateOrBulkCreate(APIView):
     def post(self, request):
         data = request.data
 
-        # Convert single object into a list for uniform processing
+       
         if isinstance(data, dict):  
             data = [data]
 
@@ -58,27 +58,27 @@ class ClientCreateOrBulkCreate(APIView):
 
         validated_data = []
         for client_data in data:
-            # Check for existing phone numbers before validating data
+            
             if Client.objects.filter(phone_number=client_data.get('phone_number')).exists():
                 return Response({'error': f"Phone number {client_data['phone_number']} already exists."}, status=status.HTTP_409_CONFLICT)
 
-            # Proceed with validation if phone number is unique
+           
             serializer = ClientSerializer(data=client_data)
             if not serializer.is_valid():
                 return Response({'error': 'Validation failed', 'details': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
             validated_data.append(serializer.validated_data)
 
-        # Assign the created_by field for all validated data
+        
         for client_data in validated_data:
             client_data['created_by'] = request.user
 
         try:
             with transaction.atomic():
-                # Bulk create clients
+               
                 clients = Client.objects.bulk_create([Client(**client_data) for client_data in validated_data])
 
-                # Serialize the created clients
+                
                 response_serializer = ClientSerializer(clients, many=True)
                 return Response({'created_clients': response_serializer.data}, status=status.HTTP_201_CREATED)
 
